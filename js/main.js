49361ea9a14888cda77132016cdf6e26ee79f0d8
ashsic <99445200+ashsic@users.noreply.github.com>
2023-10-26 20:01:02 -0400

    Completed basic JS for the game. Uses nested functions to enable proper scope

--- js/main.js
+++ js/main.js
@@ -16,33 +16,51 @@ function getPlayerChoice(){
 
 }
 
-function playRound(playerSelection, computerSelection){
-    function handleWin(){
-        console.log("You win! ${playerSelection} beats ${computerSelection}")
-    }
-    function handleLoss(){
-        console.log("You lose. ${computerSelection} beats ${playerSelection}")
-    }
-    function handleTie(){
-        console.log("Its a tie! You both picked ${computerSelection}")
-    }
-    if (playerSelection == "Rock"){
-        if (computerSelection == "Rock") handleTie();
-        if (computerSelection == "Scissors") handleWin();
-        if (computerSelection == "Paper") handleLoss();
-    } else if (playerSelection == "Paper"){
-        if (computerSelection == "Rock") handleWin();
-        if (computerSelection == "Scissors") handleLoss();
-        if (computerSelection == "Paper") handleTie();
-    } else if (playerSelection == "Scissors"){
-        if (computerSelection == "Rock") handleLoss();
-        if (computerSelection == "Scissors") handleTie();
-        if (computerSelection == "Paper") handleWin();
-    }
-}
+
 
 function playGame(){
-    console.log("Welcome to Rock Paper Scissors!");
+    function playRound(playerSelection, computerSelection){
+        function handleWin(){
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            playerScore += 1;
+        }
+        function handleLoss(){
+            console.log(`You lose. ${computerSelection} beats ${playerSelection}`);
+            computerScore += 1;
+        }
+        function handleTie(){
+            console.log(`Its a tie! You both picked ${computerSelection}`);
+        }
+        if (playerSelection == "Rock"){
+            if (computerSelection == "Rock") handleTie();
+            if (computerSelection == "Scissors") handleWin();
+            if (computerSelection == "Paper") handleLoss();
+        } else if (playerSelection == "Paper"){
+            if (computerSelection == "Rock") handleWin();
+            if (computerSelection == "Scissors") handleLoss();
+            if (computerSelection == "Paper") handleTie();
+        } else if (playerSelection == "Scissors"){
+            if (computerSelection == "Rock") handleLoss();
+            if (computerSelection == "Scissors") handleTie();
+            if (computerSelection == "Paper") handleWin();
+        }
+    }
+
+    let playerScore = 0;
+    let computerScore = 0;
+    console.log("Welcome to Rock Paper Scissors! This will be a 5 round game.");
+    console.log("Can you beat the computer?")
+    for (let i = 1; i < 6; i++){
+        playRound(getPlayerChoice(), getComputerChoice());
+        console.log(`Current scores: player ${playerScore}, computer ${computerScore}`);
+    }
+    console.log("Final score is:");
+    console.log(`player: ${playerScore}, computer: ${computerScore}`);
+    if (playerScore > computerScore){
+        console.log("You won! Great job.");
+    }else{
+        console.log("Better luck next time!");
+    }
 }
 
-getPlayerChoice();
+playGame();
