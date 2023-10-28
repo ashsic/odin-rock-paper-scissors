b0e86d410fff3e84d3899568d59fe385b17d6cb2
ashsic <99445200+ashsic@users.noreply.github.com>
2023-10-28 15:44:53 -0400

    Completed JS event listeners, style + html edits

--- js/main.js
+++ js/main.js
@@ -16,51 +16,82 @@ function getComputerChoice(){
 //     return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
 // }
 
+function handleClick(event){
+    console.log(event.target.getAttribute("id"));
+    playerSelection = event.target.getAttribute("id");
+    playRound(playerSelection, getComputerChoice());
+    keepScore();
+};
 
 
-function playGame(){
-    function playRound(playerSelection, computerSelection){
-        function handleWin(){
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-            playerScore += 1;
-        }
-        function handleLoss(){
-            console.log(`You lose. ${computerSelection} beats ${playerSelection}`);
-            computerScore += 1;
-        }
-        function handleTie(){
-            console.log(`Its a tie! You both picked ${computerSelection}`);
-        }
-        if (playerSelection == "Rock"){
-            if (computerSelection == "Rock") handleTie();
-            if (computerSelection == "Scissors") handleWin();
-            if (computerSelection == "Paper") handleLoss();
-        } else if (playerSelection == "Paper"){
-            if (computerSelection == "Rock") handleWin();
-            if (computerSelection == "Scissors") handleLoss();
-            if (computerSelection == "Paper") handleTie();
-        } else if (playerSelection == "Scissors"){
-            if (computerSelection == "Rock") handleLoss();
-            if (computerSelection == "Scissors") handleTie();
-            if (computerSelection == "Paper") handleWin();
-        }
+let playerSelection = false;
+let buttonsList = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+console.log("Welcome to Rock Paper Scissors! This will be a 5 round game.");
+console.log("Can you beat the computer?")
+
+buttonsList.forEach(function(button){
+    button.addEventListener("click", handleClick);
+});
+
+
+
+function playRound(playerSelection, computerSelection){
+    function handleWin(){
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        playerScore += 1;
     }
-
-    let playerScore = 0;
-    let computerScore = 0;
-    console.log("Welcome to Rock Paper Scissors! This will be a 5 round game.");
-    console.log("Can you beat the computer?")
-    for (let i = 1; i < 6; i++){
-        playRound(getPlayerChoice(), getComputerChoice());
-        console.log(`Current scores: player ${playerScore}, computer ${computerScore}`);
+    function handleLoss(){
+        console.log(`You lose. ${computerSelection} beats ${playerSelection}`);
+        computerScore += 1;
     }
-    console.log("Final score is:");
+    function handleTie(){
+        console.log(`Its a tie! You both picked ${computerSelection}`);
+    }
+    if (playerSelection == "Rock"){
+        if (computerSelection == "Rock") handleTie();
+        if (computerSelection == "Scissors") handleWin();
+        if (computerSelection == "Paper") handleLoss();
+    } else if (playerSelection == "Paper"){
+        if (computerSelection == "Rock") handleWin();
+        if (computerSelection == "Scissors") handleLoss();
+        if (computerSelection == "Paper") handleTie();
+    } else if (playerSelection == "Scissors"){
+        if (computerSelection == "Rock") handleLoss();
+        if (computerSelection == "Scissors") handleTie();
+        if (computerSelection == "Paper") handleWin();
+    }
+    console.log("Current score: ")
     console.log(`player: ${playerScore}, computer: ${computerScore}`);
-    if (playerScore > computerScore){
-        console.log("You won! Great job.");
-    }else{
-        console.log("Better luck next time!");
-    }
 }
 
-playGame();
+
+
+
+function keepScore(){
+    if (playerScore >= 5 || computerScore >= 5){
+        console.log("Final score is:");
+        console.log(`player: ${playerScore}, computer: ${computerScore}`);
+        if (playerScore > computerScore){
+            console.log("You won! Great job.");
+        }else{
+            console.log("Better luck next time!");
+        }
+        for (button of buttonsList){
+            button.removeEventListener("click", handleClick);
+        }
+    }
+}
+// if (playerScore >= 5 || computerScore >= 5){
+//     console.log("Final score is:");
+//     console.log(`player: ${playerScore}, computer: ${computerScore}`);
+//     if (playerScore > computerScore){
+//         console.log("You won! Great job.");
+//     }else{
+//         console.log("Better luck next time!");
+//     }
+// }
+
+
+
